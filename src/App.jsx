334b3e5b8800728fb8626d75489e94f6aa6f8cd5
Japesh Bagga <japbag2005@gmail.com>
2024-07-17 00:39:55 +0530

    url updated for live

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ const App = () => {
     const fetchUser = async () => {
       try {
         const response = await axios.get(
-          "http://localhost:4000/api/v1/user/patient/me",
+          "https://seashell-app-qeo8e.ondigitalocean.app/api/v1/user/patient/me",
           {
             withCredentials: true,
           }
