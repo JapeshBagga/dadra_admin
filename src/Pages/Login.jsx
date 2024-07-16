21d2d99bdba8050e93f5579d41dcb03bb2d31b5d
Japesh Bagga <japbag2005@gmail.com>
2024-07-17 00:54:31 +0530

    url updated for live

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -18,7 +18,7 @@ const Login = () => {
     try {
       await axios
         .post(
-          "http://localhost:4000/api/v1/user/login",
+          "https://seashell-app-qeo8e.ondigitalocean.app/api/v1/user/login",
           { email, password, confirmPassword, role: "Patient" },
           {
             withCredentials: true,
