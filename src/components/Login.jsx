bd93938cdc39b7c741df817845a80ede43468877
Japesh Bagga <japbag2005@gmail.com>
2024-07-17 01:31:00 +0530

    project updated

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,15 +1,15 @@
-import axios from "axios";
 import React, { useContext, useState } from "react";
+import { Navigate, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import { Context } from "../main";
-import { Link, useNavigate, Navigate } from "react-router-dom";
+import axios from "axios";
 
 const Login = () => {
-  const { isAuthenticated, setIsAuthenticated } = useContext(Context);
-
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  // const [confirmPassword, setConfirmPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const { isAuthenticated, setIsAuthenticated } = useContext(Context);
 
   const navigateTo = useNavigate();
 
@@ -18,8 +18,8 @@ const Login = () => {
     try {
       await axios
         .post(
-          "https://seashell-app-qeo8e.ondigitalocean.app/api/v1/user/login",
-          { email, password, password, role: "Patient" },
+          "http://localhost:4000/api/v1/user/login",
+          { email, password, confirmPassword, role: "Admin" },
           {
             withCredentials: true,
             headers: { "Content-Type": "application/json" },
@@ -31,7 +31,7 @@ const Login = () => {
           navigateTo("/");
           setEmail("");
           setPassword("");
-          // setConfirmPassword("");
+          setConfirmPassword("");
         });
     } catch (error) {
       toast.error(error.response.data.message);
@@ -44,13 +44,10 @@ const Login = () => {
 
   return (
     <>
-      <div className="container form-component login-form">
-        <h2>Sign In</h2>
-        <p>Please Login To Continue</p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat culpa
-          voluptas expedita itaque ex, totam ad quod error?
-        </p>
+      <section className="container form-component">
+        <img src="/logo.png" alt="logo" className="logo" />
+        <h1 className="form-title">WELCOME TO ZEECARE</h1>
+        <p>Only Admins Are Allowed To Access These Resources!</p>
         <form onSubmit={handleLogin}>
           <input
             type="text"
@@ -64,32 +61,17 @@ const Login = () => {
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
-          {/* <input
+          <input
             type="password"
             placeholder="Confirm Password"
             value={confirmPassword}
             onChange={(e) => setConfirmPassword(e.target.value)}
-          /> */}
-          <div
-            style={{
-              gap: "10px",
-              justifyContent: "flex-end",
-              flexDirection: "row",
-            }}
-          >
-            <p style={{ marginBottom: 0 }}>Not Registered?</p>
-            <Link
-              to={"/register"}
-              style={{ textDecoration: "none", color: "#271776ca" }}
-            >
-              Register Now
-            </Link>
-          </div>
+          />
           <div style={{ justifyContent: "center", alignItems: "center" }}>
             <button type="submit">Login</button>
           </div>
         </form>
-      </div>
+      </section>
     </>
   );
 };
